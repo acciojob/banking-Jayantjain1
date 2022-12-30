92ed021660ae42f5e710f80d79e7faa7b7b723c9
Jayantjain1 <jjayant511@gmail.com>
2022-12-30 19:44:58 +0530

    bank

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        SavingsAccount s = new SavingsAccount("BOI",23,3.0,9.0);
+        System.out.print(s.getCompoundInterest(2,3));
 
     }
 }
